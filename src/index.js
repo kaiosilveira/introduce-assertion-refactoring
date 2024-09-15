e049d3ac490462ed9c07a7cb8500ba29add386a5
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-15 07:05:33 +0100

    setup initial examples + test + CI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-export function toBeRefactored() {
-  return 'Hello, world!';
+export class Customer {
+  applyDiscount(aNumber) {
+    return this.discountRate ? aNumber - this.discountRate * aNumber : aNumber;
+  }
+
+  set discountRate(aNumber) {
+    this._discountRate = aNumber;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,37 @@
-import { toBeRefactored } from './index';
+import { Customer } from '.';
 
-describe('functionToBeRefactored', () => {
-  it('should work', () => {
-    expect(toBeRefactored()).toEqual('Hello, world!');
+describe('Customer', () => {
+  describe('applyDiscount', () => {
+    it('should return the same amount if discountRate is zero', () => {
+      const customer = new Customer();
+      customer.discountRate = 0;
+      expect(customer.applyDiscount(100)).toBe(100);
+    });
+
+    it('should return the discounted amount if discountRate is not zero', () => {
+      const customer = new Customer();
+      customer.discountRate = 0.2;
+      expect(customer.applyDiscount(100)).toBe(80);
+    });
+  });
+
+  describe('set discountRate', () => {
+    it('should set the discount rate', () => {
+      const customer = new Customer();
+      customer.discountRate = 0.2;
+      expect(customer.discountRate).toBe(0.2);
+    });
+
+    it('should set the discount rate to zero', () => {
+      const customer = new Customer();
+      customer.discountRate = 0;
+      expect(customer.discountRate).toBe(0);
+    });
+
+    it('should set the discount rate to null', () => {
+      const customer = new Customer();
+      customer.discountRate = null;
+      expect(customer.discountRate).toBe(null);
+    });
   });
 });
